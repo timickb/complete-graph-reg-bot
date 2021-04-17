be89700f871cdfd86249577c14c190721c2182ae
timickb <timikm2002@gmail.com>
2021-04-17 18:27:04 +0300

    Authorization & data validation

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ from registrator import Registrator
 
 INCORRECT_FORMAT_MESSAGE = "Некорректный формат"
 SUCCESS_MESSAGE = "Визит успешно зафиксирован!"
+SUCCESS_AUTH_MESSAGE = "Вы успешно авторизованы!"
+NOT_AUTHORIZED_MESSAGE = "Введите корректный код доступа к системе, чтобы иметь возможность регистрировать гостей."
 BOT_TOKEN = os.environ['CGRB_BOT_TOKEN']
 CREDENTIALS_FILE = 'key.json'
 SPREADSHEET_ID = os.environ['CGRB_SPREADSHEET_ID']
@@ -15,11 +17,22 @@ PASSWORD = os.environ['CGRB_PASSWORD']
 
 registrator = Registrator(CREDENTIALS_FILE, SPREADSHEET_ID, SHEET_ID)
 
+authorized_users = []
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
+
+def validate_name(name):
+    if len(name) > 20:
+        return False
+    blocked = "=!@#$%^&*()\{\}'/<>[].,"
+    for letter in blocked:
+        if letter in name:
+            return False
+    return True
 
 
 def start(update: Update, _: CallbackContext) -> None:
@@ -27,7 +40,16 @@ def start(update: Update, _: CallbackContext) -> None:
     update.message.reply_text('Hello!')
 
 def register(update: Update, _: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+    
+    if update.effective_user.id not in authorized_users:
+        if update.message.text == PASSWORD:
+            authorized_users.append(update.effective_user.id)
+            update.message.reply_text(SUCCESS_AUTH_MESSAGE)
+            return
+        else:
+            update.message.reply_text(NOT_AUTHORIZED_MESSAGE)
+            return
+
     words = update.message.text.split()
 
     if len(words) == 2:
@@ -38,6 +60,11 @@ def register(update: Update, _: CallbackContext) -> None:
         except ValueError:
             update.message.reply_text(INCORRECT_FORMAT_MESSAGE)
             return
+        
+        if not validate_name(name):
+            update.message.reply_text(INCORRECT_FORMAT_MESSAGE)
+            return
+
         registrator.register_visitor(name, '', tariff)
         update.message.reply_text(SUCCESS_MESSAGE)
         return
@@ -51,6 +78,11 @@ def register(update: Update, _: CallbackContext) -> None:
         except ValueError:
             update.message.reply_text(INCORRECT_FORMAT_MESSAGE)
             return
+
+        if (not validate_name(name)) or (not validate_name(surname)):
+            update.message.reply_text(INCORRECT_FORMAT_MESSAGE)
+            return
+
         registrator.register_visitor(name, surname, tariff)
         update.message.reply_text(SUCCESS_MESSAGE)
         return
