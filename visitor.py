8b2c05e47c199ec4554205d4ae82239beb3b67f9
Timur Batrutdinov <timikm2002@gmail.com>
2021-04-25 11:30:40 +0300

    Изменен способ генерации времени визита
    Теперь это делается по-человечески

--- visitor.py
+++ visitor.py
@@ -1,12 +1,13 @@
-import datetime
+from datetime import datetime
+
 
 class Visitor:
     def __init__(self, name, surname, tariff):
         self.tariff = str(tariff).replace(".", ",")
         self.name = name
         self.surname = surname
-        self.visitMonthDay = datetime.datetime.now().day
-        self.visitTime = f'{datetime.datetime.now().time().hour}:{datetime.datetime.now().time().minute}'
+        self.visitMonthDay = datetime.now().day
+        self.visitTime = datetime.now().strftime("%H:%M")
     
     def get_sheet_row(self):
         return [str(self.visitMonthDay), 
